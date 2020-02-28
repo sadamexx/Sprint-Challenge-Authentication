4a3ca4f67c0927fc3308b850e1ab9a4a826a8702
Sarah Elias <sarah@Sarahs-MacBook-Pro.local>
2020-02-28 10:08:09 -0800

    register, login and jokes routes work

--- api/server.js
+++ api/server.js
@@ -1,18 +1,46 @@
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
+const session = require('express-session');
+const KnexStore = require('connect-session-knex')(session);
 
 const authenticate = require('../auth/authenticate-middleware.js');
 const authRouter = require('../auth/auth-router.js');
 const jokesRouter = require('../jokes/jokes-router.js');
+const knex = require('../database/dbConfig.js');
 
 const server = express();
+
+const sessionConfig = {
+    name: 'cookieMonster',
+    secret: 'chocolateChip',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        maxAge: 1000 * 60 * 10,
+        secure: false,
+        httpOnly: true,
+    },
+    store: new KnexStore({
+        knex: knex,
+        tablename: 'sessions',
+        createtable: true,
+        sidfieldname: 'sid',
+        clearInterval: 1000 * 60 * 15
+    })
+}
 
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
+server.use(session(sessionConfig));
 
 server.use('/api/auth', authRouter);
 server.use('/api/jokes', authenticate, jokesRouter);
+
+server.get('/', (req, res) => {
+    console.log(req.session);
+    res.send({server: 'up'});
+})
 
 module.exports = server;
--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,55 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+const Users = require('./auth-model.js');
+
 
 router.post('/register', (req, res) => {
-  // implement registration
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 10);
+  user.password = hash;
+
+  Users.add(user)
+  .then(saved => {
+    req.session.loggedIn = true;
+
+    res.status(201).json(saved);
+  })
+  .catch(error => {
+    res.status(500).json({message: "Unable to register, please try again"})
+  });
 });
 
 router.post('/login', (req, res) => {
-  // implement login
+  let { username, password } = req.body;
+
+  Users.findBy({ username })
+  .first()
+  .then(user => {
+    if(user && bcrypt.compareSync(password, user.password)){
+      req.session.loggedIn = true;
+      req.session.username = user.username;
+      res.status(200).json({message: `Welcome ${user.username}!`})
+    } else {
+      res.status(401).json({ message: "Invalid Credentials"});
+    }
+  })
+  .catch(error => {
+    res.status(500).json({error});
+  });
+});
+
+router.get('/logout', (req, res) => {
+  if(req.session){
+    req.session.destroy(error => {
+      if(err){
+        res.status(500).json({ message: "You are still logged in, sorry!"});
+      } else {
+        res.status(200).json({ message: "You successfully logged out! Come back soon!"});
+      }
+    });
+  } else {
+    res.status(200).json({ message: "You successfully logged out! Come back soon!"});
+  }
 });
 
 module.exports = router;
